d4a17a3a3fcc2946a44f05b546c6b953a314d6ae
Gabriel <gabriel.scalione@vanzolini.com.br>
2020-11-23 09:06:50 -0300

    Update VW_FCAV_LINK_OFERTA_CURSO_COMPLETA.sql

--- lyceum_scripts/Views/VW_FCAV_LINK_OFERTA_CURSO_COMPLETA.sql
+++ lyceum_scripts/Views/VW_FCAV_LINK_OFERTA_CURSO_COMPLETA.sql
@@ -12,6 +12,7 @@ SELECT
         WHEN CURSO.FACULDADE = 'CAPAC' THEN 'Capacitação'
         WHEN CURSO.FACULDADE = 'ESPEC' THEN 'Especialização'
         WHEN CURSO.FACULDADE = 'PALES' THEN 'Palestra'
+		WHEN CURSO.FACULDADE = 'DIFUS' THEN 'Palestra'
     END AS AREA,
     CASE
         WHEN OFERTA.CONCURSO IS NULL THEN V.TURMA
@@ -35,7 +36,7 @@ SELECT
         WHEN V.DT_INICIO IS NULL THEN '01/01/1900'
         ELSE CONVERT(datetime, V.DT_INICIO)
     END AS INICIO_DO_CURSO,
-    'http://sga.vanzolini.org.br:8080/web/converte_link_portal.asp' +
+    'https://sga.vanzolini.org.br/web/converte_link_portal.asp' +
     '?OFERTA_DE_CURSO=' + CONVERT(varchar, OFERTA.OFERTA_DE_CURSO) +
     '&tagturma=' + REPLACE(ISNULL(V.TURMA, OFERTA.CONCURSO), ' ', '%20') AS LINK
 FROM LY_OFERTA_CURSO AS OFERTA
