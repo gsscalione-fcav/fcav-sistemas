e4315a6421752066b4bc202af8c675bbcd46b895
Gabriel <gabriel.scalione@vanzolini.com.br>
2020-11-23 09:06:42 -0300

    Update SC_FCAV_TESTE DE LINK CERTIFICADO.sql

--- lyceum_scripts/Scripts/SC_FCAV_TESTE DE LINK CERTIFICADO.sql
+++ lyceum_scripts/Scripts/SC_FCAV_TESTE DE LINK CERTIFICADO.sql
@@ -1,8 +1,8 @@
-select * from  certificados 
+select * from  LYCEUM_MEDIA.dbo.certificados  
 where ALUNO = 'C201800300'
 
 
-update certificados 
+update LYCEUM_MEDIA.dbo.certificados 
 set
 	URL_APROVACAO = 'http://10.0.10.14/tstvalidade/teste_pdf.asp?B0657EFBDFC8329D96A77D8CBE192CF4'
 
@@ -19,3 +19,12 @@ from
 where aluno = 'C201800300'
 
 delete LYCEUM.DBO.LY_AVISO where aluno = 'C201800300'
+
+
+
+INSERT INTO LYCEUM.DBO.LY_AVISO
+		(ALUNO,DTINI,DTFIM,MENSAGEM,CURSO,SERIE,TIPO_AVISO,UNID_RESPONSAVEL,UNID_FISICA,
+			TURNO,CURRICULO,CONCURSO,DATA_INCLUSAO,USUARIO,DESTINO,ORDEM,LOTE,ANEXO_ID)
+		VALUES
+		('A201400091',CONVERT(DATE,GETDATE(),102),CONVERT(DATE,GETDATE()+500,102),'<p><a href="http://10.0.10.14/tstvalidade/teste_pdf.asp?B0657EFBDFC8329D96A77D8CBE192CF4">Link para o Certificado do Curso</a></p>',
+			NULL,NULL,'I',NULL,NULL,NULL,NULL,NULL,CONVERT(DATE,GETDATE(),102),'zeus',NULL,NULL,NULL,NULL)	
